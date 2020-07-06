7807a4fc0ef3af3d85e4d413e51b7456be51b740
Iftekhar <59515420+Nibroo@users.noreply.github.com>
2020-07-07 01:05:00 +0600

    Update GuessingGame.py

--- whileColt/GuessingGame.py
+++ whileColt/GuessingGame.py
@@ -2,28 +2,31 @@ import random
 
 rand_num = random.randint(1,31)
 
-def guess_in():                                                 #* TO MAKE SURE PLAYER INSERTS A NUMBER BETWEEN 1 and 30
-    guess_num = input('Please guess a number between 1 to 30: ')
+def guess_in():                            #* TO MAKE SURE PLAYER INSERTS A NUMBER BETWEEN 1 and 30
+    guess_num = input('Guess a number between 1 to 30: ')
 
     while guess_num not in range(1,31):
         guess_num = input('Please guess a number between 1 to 30: ')
-
     else:
         return int(guess_num)
 
 
-def guessing(guess_num):                                                 #? TO MAKE THE LOOP RUN UNTIL THE GUESS IS PERFECT
+def guessing(guess_num):                   #? TO MAKE THE LOOP RUN UNTIL THE GUESS IS PERFECT
     while rand_num != guess_num:
-        if rand_num > guess_num:
-            print('Too Low! Try Again')
-        elif rand_num < guess_num:
-            print('Too High! Try Again')
+        if abs(rand_num - guess_num) >= 10:
+            print('Too COLD! Try Harder')
+        elif abs(rand_num - guess_num) >= 5:
+            print('WARM! Try few more')
+        elif abs(rand_num - guess_num) >= 3:
+            print('WARMER! Try once more')
+        elif abs(rand_num - guess_num) >= 1:
+            print('Too HOT! I hope you get it this time')
         else:
-            print('Not even close bruh!')
+            print('STOP guessing bruh!')
         
-        guess_num = int(input('Please guess a number between 1 to 30: '))
+        guess_num = int(input('Please keep Guessing: '))
     else:
         print('You Won, FINALLY!')
 
-guess_in()
+guess_num = guess_in()
 guessing(guess_num)

67e24300fcbbe0be95cd18a58f5cdb716cd40e7f
Iftekhar <59515420+Nibroo@users.noreply.github.com>
2020-07-06 19:36:50 +0600

    File rearranged

---
+++ helper.py
@@ -0,0 +1,27 @@
+# def up_low(nam):
+#     resu = []
+#     resl = []
+
+#     for i in nam:
+#         if i.isupper():
+#             resu.append(i)
+#         elif i.islower():
+#             resl.append(i)
+#         else:
+#             pass
+    
+#     up = len(resu)
+#     low = len(resl)
+
+#     print('No. of Upper case characters : {}\nNo. of Lower case Characters : {}'.format(up,low))
+
+
+# nam = 'My name is Nibir'
+# up_low(nam)
+#?--------------------------------------------------------------------------------------------------
+
+# i = 5
+# i =+ 1
+# print(i)
+#?--------------------------------------------------------------------------------------------------
+

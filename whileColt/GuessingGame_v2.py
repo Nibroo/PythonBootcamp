df45cc32bd9f48e2ebb72f4d94e88e55a77739b7
Iftekhar <59515420+Nibroo@users.noreply.github.com>
2020-07-07 02:22:53 +0600

    v2 of GuessingGame

--- whileColt/GuessingGame_v2.py
+++ whileColt/GuessingGame_v2.py
@@ -2,31 +2,63 @@ import random
 
 rand_num = random.randint(1,31)
 
-def guess_in():                            #* TO MAKE SURE PLAYER INSERTS A NUMBER BETWEEN 1 and 30
+def nums():                                 #? TO MAKE LIST OF STRING INTEGARS
+    nums = []
+    for i in range(1,31):
+        nums.append(str(i))
+
+    return nums
+
+
+def guess_in(nums):                        #* TO MAKE SURE PLAYER INSERTS A NUMBER BETWEEN 1 and 30
     guess_num = input('Guess a number between 1 to 30: ')
 
-    while guess_num not in range(1,31):
-        guess_num = int(input('Please guess a number between 1 to 30: '))
+    while guess_num not in nums:
+        guess_num = input('Please guess a number between 1 to 30: ')
     else:
         return int(guess_num)
 
 
 def guessing(guess_num):                   #? TO MAKE THE LOOP RUN UNTIL THE GUESS IS PERFECT
     while rand_num != guess_num:
-        if abs(rand_num - guess_num) >= 10:
-            print('Too COLD! Try Harder')
-        elif abs(rand_num - guess_num) >= 5:
-            print('WARM! Try few more')
-        elif abs(rand_num - guess_num) >= 3:
-            print('WARMER! Try once more')
-        elif abs(rand_num - guess_num) >= 1:
-            print('Too HOT! I hope you get it this time')
+        diff = abs(rand_num - guess_num)
+        if diff >= 10:
+            guess_num = input('Too COLD! Try Harder: ')
+        elif diff >= 5:
+            guess_num = input('WARM! Try few more: ')
+        elif diff >= 3:
+            guess_num = input('WARMER! Try once more: ')
+        elif diff >= 1:
+            guess_num = input('Too HOT! I hope you get it this time: ')
         else:
             print('STOP guessing bruh!')
         
-        guess_num = int(input('Please keep Guessing: '))
+        guess_num = int(guess_num)    
     else:
         print('You Won, FINALLY!')
 
-guess_num = guess_in()
-guessing(guess_num)
+
+def gameon_choice():                        #* TO KEEP PLAYING OR QUITTING
+    choice = 'Wrong'
+
+    while choice not in ['y','n']:
+        choice = input('Wanna keep playing? (Y/N): ').lower()
+
+        if choice not in ['y','n']:
+            print("Sorry! can't accept, Choose Y or N")
+        
+    if choice == 'y':
+        return True
+    else:
+        print('Okay! See you again!')
+        return False
+#?------------------------------------------------------
+                    #! GAME PLAY
+#?------------------------------------------------------
+game_on = True
+nums = nums()
+
+while game_on:
+    guess_num = guess_in(nums)
+    guessing(guess_num)
+    game_on = gameon_choice()
